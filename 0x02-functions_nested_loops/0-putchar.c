85b8f16caca6a8af51529614458af7ab81728b93
Emmyvick <emmyvic412@gmail.com>
2022-07-05 11:49:15 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,11 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
 * main-Entry point
 *
 * Return: 0 if successful
 */
-int main(void)
+int _putchar(char c)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+return (write(1, &c, 1));
 }
