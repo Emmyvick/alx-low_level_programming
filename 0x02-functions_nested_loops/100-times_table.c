cbe7e33cb28a55a5e8396b2363ffda27ee548063
Emmyvick <emmyvic412@gmail.com>
2022-07-05 16:30:56 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,52 @@
 #include "main.h"
 
 /**
- * print_times_table - prints time table
- * @n : use in times table
+ * print_times_table -  times table
+ * @n : times table to use
+ * Description: print times table
  * Return:void
  */
-void print_times_table(int n)
+
+void print_times_table(int n) 
 {
-	int a = 0, rep, b;
 
-	if (n < 0 || n > 15)
-		return;
+int a = 0, rep, b;
 
-	while (a <= n)
-	{
-		for (b = 0; b <= n; b++)
-		{
-			rep = a * b;
-			if (b == 0)
-				_putchar('0' + rep);
-			else if (rep < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + rep);
-			}
-			else if (rep < 100)
-			{
-				_putchar (' ');
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
-			}
-			else
-			{
-				_putchar('0' + rep / 100);
-				_putchar('0' + (rep - 100) / 10);
-				_putchar('0' + rep % 10);
-			}
-			if (b < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-		a++;
-	}
+if (n < 0 || n >15)
+return;
+
+while(a <= n)
+{
+for (b = 0; b <= n; b++)
+{
+rep = a * b;
+if (b == 0)
+_putchar('0' + rep);
+else if (rep < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + rep);
+}
+else if (rep < 100)
+{
+_putchar(' ');
+_putchar('0' + rep / 10);
+_putchar('0' + rep % 10);
+}
+else 
+{
+_putchar('0' + rep / 100);
+_putchar('0' + (rep - 100) / 100);
+_putchar('0' + rep % 10);
+ }
+if (b < n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+a++;
+}
 }
