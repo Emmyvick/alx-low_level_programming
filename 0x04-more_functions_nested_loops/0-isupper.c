f377407dbd70b9e4e28be53d0d5595e372500038
Emmyvick <emmyvic412@gmail.com>
2022-07-23 03:53:07 -0700

    Task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,32 @@
 #include "main.h"
+
+#include <ctype.h>
+
+
+
 /**
- * _isupper-evaluate if letter is uppercase
- *@c: print int
- * Return: 0 if successful
- */
+* _isupper - checks for uppercase letters
+*
+* @c: parametr to be printed
+*
+* Return: 0 or 1 depending on the condition
+*/
+
+
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
+	if (isupper(c))
+	{
 
-else
-{
-return (0);
-}
+		return (1);
+
+	}
+	else
+	{
+
+		return (0);
+
+	}
 
 }
